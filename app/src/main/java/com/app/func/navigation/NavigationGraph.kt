3c0c1c1ef4219a5ef7df9c0415304717afcc4e74
H Ngg <hnn@gmail.com>
2025-03-18 22:37:43 +0700

    - Refactor, remove redundant code, textInputLayout

--- app/src/main/java/com/app/func/navigation/NavigationGraph.kt
+++ app/src/main/java/com/app/func/navigation/NavigationGraph.kt
@@ -1,29 +1,7 @@
 package com.app.func.navigation
 
-import androidx.core.os.bundleOf
-import androidx.navigation.NavController
-import kotlinx.serialization.Serializable
-
 enum class Screen {
     MAIN_ANIMATION,
     BUBBLE_ANIMATION,
     BUBBLE_EMITTER_ANIMATION
 }
-
-@Serializable
-object MainAnimation
-
-@Serializable
-object BubbleAnimation
-
-@Serializable
-object BubbleEmitterAnimation
-
-@Serializable
-data class Main(val id: String)
-
-@Serializable
-data class Bubble(val id: String)
-
-@Serializable
-data class Emitter(val id: String)
